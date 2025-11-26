6d74c1bcdb76970e53f1b889883c9e276e9ee2a6
Farid Syaiful <miztallizer@gmail.com>
2025-11-26 09:52:45 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+// config.php
+return [
+    'db' => [
+        'host' => '127.0.0.1',
+        'dbname' => 'xor_crud',
+        'user' => 'root',
+        'pass' => '',
+        'charset' => 'utf8mb4'
+    ],
+    'crypto_key' => 'mysecretkey'
+];
